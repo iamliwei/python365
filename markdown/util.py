cbd9025a965ef44114f88c779b6a001293e4952f
iamliwei <543774800@qq.com>
2019-03-07 22:13:19 +0800

    markdown转换工具，，重复执行

--- markdown/util.py
+++ markdown/util.py
@@ -1,14 +1,14 @@
 # 文本块生成器
 
 def lines(file):
-    for line in file:yield line
+    for line in file: yield line
     yield '\n'
 
 def blocks(file):
-    block=[]
+    block = []
     for line in lines(file):
         if line.strip():
             block.append(line)
         elif block:
             yield ''.join(block).strip()
-            block=[]
+            block = []

b4de98e7fc7fc7bf889b3c7f4c75476d8e631a9d
iamliwei <543774800@qq.com>
2019-03-07 17:01:48 +0800

    markdown转换工具

---
+++ chapter/test.py
@@ -0,0 +1,10 @@
+class Person:
+    def set_name(self,name):
+        self.name=name
+
+    def get_name(self,name):
+        self.name=name
+
+    def greet(self):
+        print("hello,world! I'm{}.".format(self.name))
+
